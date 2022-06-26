81da030e78523286c6a079d025fbd0d9d835c7b8
rteklu <rahelteklu@gmail.com>
2022-06-25 22:02:25 -0500

    lab4

---
+++ lab5/js/appLab5Prob1.js
@@ -0,0 +1,7 @@
+"use strict";
+const strArry =[1,2,3,4,5];
+function sum(strArry){
+    const sum =strArry[i];
+    const a = sum.filter(sum>20);
+}
+return a;
---
+++ lab5/js/appLab5Prob2.js
@@ -0,0 +1,8 @@
+"use strict";
+const a=strArry(1, "john", 4);
+function getNewArry(a){
+    const arr=a.filter((a.length>5)&&(a.length===5));
+    const result= newArray().includes(arr);
+}
+console.log(result);
+return result;
